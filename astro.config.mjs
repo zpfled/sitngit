f1a4bb561db1632d31de70e5c849342db8fc14de
Zach Pflederer <zpfled@gmail.com>
2026-01-24 12:48:37 -0600

    use static config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://sitandgit.com",
   integrations: [tailwind(), sitemap()],
-  output: "hybrid",
+  output: "static",
   trailingSlash: "never"
 });
