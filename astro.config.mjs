fd74be77a283bd882e00174e8f4d325102b54f04
Zach Pflederer <zpfled@gmail.com>
2026-01-24 16:37:08 -0600

    seo tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://sitandgit.com",
+  site: "https://www.sitandgit.com",
   integrations: [tailwind()],
   output: "static",
   trailingSlash: "never"
