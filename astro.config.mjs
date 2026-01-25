4d998661b91f1bfa575e059bae2e96dd73f8cc76
Zach Pflederer <zpfled@gmail.com>
2026-01-25 09:37:02 -0600

    canonical name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://www.sitandgit.com",
+  site: "https://sitandgit.com",
   integrations: [tailwind()],
   output: "static",
   trailingSlash: "never"
